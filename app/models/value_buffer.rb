5e58a722ee5481183939be444b496f622dfadfee
Vikko Smit <vikkosmi@hotmail.com>
2012-10-03 14:23:14 +0200

    rewrite

--- app/models/value_buffer.rb
+++ app/models/value_buffer.rb
@@ -1,23 +1,27 @@
 class ValueBuffer
-  BUFFER = []
-  MAX_BUFFER_SIZE = 5
+  attr_accessor :array, :max_buffer_size
+  
+  def initialize
+    @array = []
+    @max_array_size = 5
+  end  
   
   def self.add(input)
     # hash = {input}
-    BUFFER << input #hash possible
-    while BUFFER.size > MAX_BUFFER_SIZE
-      BUFFER.delete_at(0) #drop first packets until buffer size <= 10
+    @array << input #hash possible
+    while @array.size > @max_array_size
+      @array.delete_at(0) #drop first packets until buffer size <= 10
     end
-    return BUFFER
+    return @array
   end
   
   def self.size
-    return BUFFER.size
+    return @array.size
   end
   
   def self.get
-    if BUFFER.size > 0
-      return BUFFER.delete_at(0)
+    if @array.size > 0
+      return @array.delete_at(0)
     else
       return nil
     end
--- lib/tasks/listener.rake
+++ lib/tasks/listener.rake
@@ -2,16 +2,19 @@ desc 'Listens to pipe and does stuff'
 task :listen_to_pipe => :environment do
   puts "Listen to Pipe starting"
   #mkfifo /tmp/pipe
-  input = File.open("/tmp/pipe", "w+")
+  input = File.open("/tmp/pipe", "r+")
+  buffer = ValueBuffer.new
   READ = 1
   i = 0 
   delay = 10
   while READ
-    # ValueBuffer.add(input.gets)
+    buffer.add(input.gets)
     sleep (delay.to_f / 1000)
-    if ((i % 500) == 0)
+    if ((i % 100) == 0)
       puts "listening to pipe for " + (i*delay / 1000).to_s + "s."
+      puts "current buffer: " + buffer.array.to_s
     end
     i += 1
+
   end
 end
