253ee0de28df8007077e6275351ef2f6cd445259
vikko <vikkosmi@hotmail.com>
2012-10-09 22:18:51 +0200

    removed database requirement

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all' Dont need active record
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
+require "sprockets/railtie"
+#require standalones instead
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
