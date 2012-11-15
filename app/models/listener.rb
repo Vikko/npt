bf67c6eabc19fbcf75176bfdf48ccfd2bcb29de2
vikko <vikkosmi@hotmail.com>
2012-11-15 14:45:44 +0100

    working locally

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -15,31 +15,18 @@ class Listener
   end
   
   def listen_thread
-    i = 0 
     while @read == 1
       @delay = 100
       buffer = 10.seconds
       to = Time.now - buffer
       from = to - 0.1
-      ActiveRecord::Base.connection_pool.with_connection do 
-        puts(" == Getting data == ")
-        data = RawMeasurement.between(from, to)
-        puts(data.inspect)
-      end
+      # puts(" == Getting data == ")
+      data = RawMeasurement.between(from, to)
+      # puts(data.inspect)
       data.each do |entry|
         @buffer.add("0,#{entry.value1},#{entry.value2}")
       end
-      # if data.present?
-      #   prev = data
-      # else 
-      #   data = prev
-      # end
-      # @buffer.add(data.) 
       sleep (@delay.to_f / 1000)
-      # if ((i % 50) == 0)
-      # puts "COUNT: #{i}"
-      # end
-      i += 1
       ActiveRecord::Base.verify_active_connections!() if defined? ActiveRecord
     end
   end
