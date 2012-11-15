98e33fd602dcb2b828b90c2752e43705e84cd071
vikko <vikkosmi@hotmail.com>
2012-11-15 13:31:37 +0100

    accelerometer

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -35,7 +35,7 @@ class SensorsController < ApplicationController
   end
   
   def post_data
-    measure = RawMeasurement.new(:time => Time.now, :type => 1, :value1 => params[:EMG], :value2 => params[:EMG])
+    measure = RawMeasurement.new(:time => Time.now, :type => 1, :value1 => params[:AccelerometerX], :value2 => params[:AccelerometerY], :value3 => params[:AccelerometerZ])
     if measure.valid?
       measure.save
     end
--- app/models/raw_measurement.rb
+++ app/models/raw_measurement.rb
@@ -1,5 +1,6 @@
 class RawMeasurement < ActiveRecord::Base
   attr_accessor :time, :type, :value1, :value2, :value3
+  validates_presence_of :time, :type, :value1, :value2, :value3 
   
   scope :between, lambda {|from,to| where("time > ? AND time < ?", from, to)}
 end
