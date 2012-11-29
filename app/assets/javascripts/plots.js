02dcb5fc3c6608a69d65476bc37ccce9509eed0d
vikko <vikkosmi@hotmail.com>
2012-11-29 14:46:14 +0100

    missing css & js

---
+++ app/assets/javascripts/plots.js
@@ -0,0 +1,136 @@
+// global vars & initialize vars
+started = false;
+updateInterval = 500;
+heart_rate_data = [];
+bpm_rate_data = [];
+sweat_data = [];
+muscle_data = [];
+accelero_data = [];
+gyro_data = [];
+intervalId = "";
+delay = 5;
+
+//init script
+$(document).ready(function($){
+	setup_plots();
+	draw_live_heartrate();
+	draw_bpmrate();
+	draw_sweat();
+	draw_muscle();
+	draw_accel();
+	draw_gyro();
+	$("#start").click(function(e){
+		start();
+	});
+	$("#stop").click(function(e){
+		stop();
+	});
+});
+function get_data() {
+	$.ajax({
+		url: "sensors/get_data",
+		dataType: "script" 
+	});
+}
+
+// flot
+function setup_plots() {
+  	// live heart rate
+		options_lhr = {
+        series: { shadowSize: 0 },
+        yaxis: { show: false},
+				color: "#FF0000", 
+        xaxis: { show: false }
+    };
+		// beats per minute
+		options_bpm = {
+	      series: { shadowSize: 0 },
+				color: "#FF0000", 
+	      yaxis: { min: 0, max: 180 },
+	      xaxis: { show: false }
+    };
+  	// sweat
+		options_sw = {
+				series: { shadowSize: 0 },
+				yaxis: { show: false},
+				color: "#FF0000", 
+				xaxis: { show: false }
+    };
+  	// muscle tension
+		options_mt = {
+				series: { shadowSize: 0 },
+				yaxis: { show: false},
+				color: "#FF0000", 
+				xaxis: { show: false }
+    };
+  	// accelerometer
+		options_ac = {
+				series: { shadowSize: 0 },
+				yaxis: { show: false},
+				color: "#FF0000", 
+				xaxis: { show: false }
+    };
+  	// gyroscope
+		options_gy = {
+				series: { shadowSize: 0 },
+				yaxis: { show: false},
+				color: "#FF0000", 
+				xaxis: { show: false }
+    };
+
+}
+
+function start() {
+	if (!(started)) {
+		started = true;
+		if (intervalId == "") {
+			intervalId = setInterval(function(){
+				get_data();
+				draw_live_heartrate();
+				draw_sweat();
+				draw_muscle();
+				draw_accel();
+				draw_gyro();
+			}, updateInterval);
+			console.log("Started updating plot");
+		} else {
+			console.log("Called start, but already started.")
+		}
+	}
+}
+
+function stop() {
+	if (started) {
+		started = false;
+		if (intervalId == "") {
+			console.log("Called stop, but not running.")
+		} else {
+			clearInterval(intervalId);
+			intervalId = "";
+		}
+	}
+}
+
+function draw_live_heartrate() {
+	$.plot($("#live_heart_rate_plot"), [ heart_rate_data ], options_lhr);
+}
+
+function draw_bpmrate() {
+	$.plot($("#bpm_rate_plot"), [ bpm_rate_data ], options_bpm);
+}
+
+function draw_sweat() {
+	$.plot($("#sweat_plot"), [ sweat_data ], options_sw);
+}
+
+function draw_muscle() {
+	$.plot($("#muscle_tension_plot"), [ muscle_data ], options_mt);
+}
+
+function draw_accel() {
+	$.plot($("#accelerometer_plot"), [ accelero_data ], options_ac);
+}
+
+function draw_gyro() {
+	$.plot($("#gyroscope_plot"), [ gyro_data ], options_gy);
+}
