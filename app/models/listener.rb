447ebcc9624844a8fac316a5cfbb859ba7d488ca
vikko <vikkosmi@hotmail.com>
2013-03-07 15:00:05 +0100

    0 delay

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -26,7 +26,7 @@ class Listener
   def listen_thread
     while @read == 1
       @delay = 500
-      buffer = (3.5).seconds
+      buffer = 0.seconds
       to = Time.now - buffer
       from = to - (@delay.to_f / 1000)
       data = RawMeasurement.between(from, to)
