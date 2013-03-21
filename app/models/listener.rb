d2c98054e9da9bad78645b4085d03d2883082c68
vikko <vikkosmi@hotmail.com>
2013-03-21 13:35:55 +0100

    Rails.cacheg

--- app/models/listener.rb
+++ app/models/listener.rb
@@ -5,56 +5,76 @@ class Listener
   attr_accessor :read, :delay
   
   def initialize
-    Thread.main[:hr_buffer] = ValueBuffer.new(HEARTRATE)
-    Thread.main[:sw_buffer] = ValueBuffer.new(SWEAT)
-    Thread.main[:mt_buffer] = ValueBuffer.new(MUSCLETENSION)
-    Thread.main[:accel_buffer] = ValueBuffer.new(ACCELEROMETER)
-    Thread.main[:gyro_buffer] = ValueBuffer.new(GYROSCOPE)
-    Thread.main[:geo_buffer] = ValueBuffer.new(GEOLOCATION)
-    Thread.main[:resp_buffer] = ValueBuffer.new(RESPIRATION)
-    Thread.main[:peac_buffer] = ValueBuffer.new(PEAKACCEL)
-    Thread.main[:post_buffer] = ValueBuffer.new(POSTURE)
-    Thread.main[:eeg_buffer] = ValueBuffer.new(EEG)
+    unless Rails.cache[:hr_buffer].present?
+      Rails.cache[:hr_buffer] = ValueBuffer.new(HEARTRATE)
+    end
+    unless Rails.cache[:sw_buffer].present?
+      Rails.cache[:sw_buffer] = ValueBuffer.new(SWEAT)
+    end
+    unless Rails.cache[:mt_buffer].present?
+      Rails.cache[:mt_buffer] = ValueBuffer.new(MUSCLETENSION)
+    end
+    unless Rails.cache[:accel_buffer].present?
+      Rails.cache[:accel_buffer] = ValueBuffer.new(ACCELEROMETER)
+    end
+    unless Rails.cache[:gyro_buffer].present?
+      Rails.cache[:gyro_buffer] = ValueBuffer.new(GYROSCOPE)
+    end
+    unless Rails.cache[:geo_buffer].present?
+      Rails.cache[:geo_buffer] = ValueBuffer.new(GEOLOCATION)
+    end
+    unless Rails.cache[:resp_buffer].present?
+      Rails.cache[:resp_buffer] = ValueBuffer.new(RESPIRATION)
+    end
+    unless Rails.cache[:peac_buffer].present?
+      Rails.cache[:peac_buffer] = ValueBuffer.new(PEAKACCEL)
+    end
+    unless Rails.cache[:post_buffer].present?
+      Rails.cache[:post_buffer] = ValueBuffer.new(POSTURE)
+    end
+    unless Rails.cache[:eeg_buffer].present?
+      Rails.cache[:eeg_buffer] = ValueBuffer.new(EEG)
+    end
   end
   
   def hr_buffer
-    Thread.main[:hr_buffer]
+    Rails.cache[:hr_buffer]
   end
   
   def sw_buffer
-    Thread.main[:sw_buffer]
+    Rails.cache[:sw_buffer]
   end
   
   def mt_buffer
-    Thread.main[:mt_buffer]
+    Rails.cache[:mt_buffer]
   end
   
   def accel_buffer
-    Thread.main[:accel_buffer]
+    Rails.cache[:accel_buffer]
   end
   
   def gyro_buffer
-    Thread.main[:gyro_buffer]
+    Rails.cache[:gyro_buffer]
   end
   
   def geo_buffer
-    Thread.main[:geo_buffer]
+    Rails.cache[:geo_buffer]
   end
   
   def resp_buffer
-    Thread.main[:resp_buffer]
+    Rails.cache[:resp_buffer]
   end
   
   def peac_buffer
-    Thread.main[:peac_buffer]
+    Rails.cache[:peac_buffer]
   end
   
   def post_buffer
-    Thread.main[:post_buffer]
+    Rails.cache[:post_buffer]
   end
   
   def eeg_buffer
-    Thread.main[:eeg_buffer]
+    Rails.cache[:eeg_buffer]
   end
   
   def listen
@@ -73,25 +93,25 @@ class Listener
       data.each do |entry|
         case entry.sensor_type
         when HEARTRATE
-          Thread.main[:hr_buffer].add(entry)
+          Rails.cache[:hr_buffer].add(entry)
         when SWEAT
-          Thread.main[:sw_buffer].add(entry)
+          Rails.cache[:sw_buffer].add(entry)
         when MUSCLETENSION
-          Thread.main[:mt_buffer].add(entry)
+          Rails.cache[:mt_buffer].add(entry)
         when ACCELEROMETER
-          Thread.main[:accel_buffer].add(entry)
+          Rails.cache[:accel_buffer].add(entry)
         when GYROSCOPE
-          Thread.main[:gyro_buffer].add(entry)
+          Rails.cache[:gyro_buffer].add(entry)
         when GEOLOCATION
-          Thread.main[:geo_buffer].add(entry)
+          Rails.cache[:geo_buffer].add(entry)
         when RESPIRATION
-          Thread.main[:resp_buffer].add(entry)
+          Rails.cache[:resp_buffer].add(entry)
         when PEAKACCEL
-          Thread.main[:peac_buffer].add(entry)
+          Rails.cache[:peac_buffer].add(entry)
         when POSTURE
-          Thread.main[:post_buffer].add(entry)
+          Rails.cache[:post_buffer].add(entry)
         when EEG
-          Thread.main[:eeg_buffer].add(entry)
+          Rails.cache[:eeg_buffer].add(entry)
         end
       end
       sleep (@delay.to_f / 1000)
